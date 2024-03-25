405a68cf63a71465efae6f81f0b0bffeb49dfff7
aridspace10 <jackovand27@icloud.com>
2024-03-25 21:44:59 +1000

    Program can now open up basic window

--- SpotifyClone.py
+++ SpotifyClone.py
@@ -1,16 +1,36 @@
-
+import tkinter as tk
+class HomeView(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        # Add widgets for the home page
 class Model():
-    def __init__(self) -> None:
+    def __init__(self):
         pass
 
 class View():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, master):
+        self.master = master
+        self.current_page = None
+
+    def open(self, page_class):
+        if self.current_page:
+            self.current_page.destroy()
+        # Create new page
+        self.current_page = page_class(self.master)
+        self.current_page.pack()
+
 class Controller():
-    def __init__(self) -> None:
+    def __init__(self):
         self.model = Model()
-        self.view = View()
+        self.root = tk.Tk()
+        self.view = View(self.root)
         self.run()
+        self.root.mainloop()
     
-    def run(self) -> None:
-        pass
+    def run(self):
+        self.view.open(HomeView)
+
+controller = Controller()
+controller.run()
