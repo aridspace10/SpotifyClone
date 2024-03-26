5ba1e0ed4cd142d6b032a3d491a36fb84d99a0c3
aridspace10 <jackovand27@icloud.com>
2024-03-26 21:06:50 +1000

    Added script to create an artist, user, playlist

--- CreateDB.sql
+++ CreateDB.sql
@@ -2,6 +2,21 @@ CREATE TABLE artists (
     id PRIMARY KEY auto_increment NOT NULL,
     fname text NOT NULL, 
     lname text, 
-    DOB date NOT NULL, 
-    
+    DOB date NOT NULL
+)
+
+CREATE TABLE user (
+    id PRIMARY KEY auto_increment NOT NULL,
+    username text NOT NULL,
+    email text NOT NULL,
+    DOC date NOT NULL, 
+    password text NOT NULL
+)
+
+CREATE TABLE playlist (
+    id PRIMARY KEY auto_increment NOT NULL,
+    name text NOT NULL,
+    DOC date NOT NULL,
+    userid int NOT NULL, 
+    FOREIGN KEY (userid) REFERENCES user(id)
 )
