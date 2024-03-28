03a23d21a506028804edc027eac104d44d9bcd0a
aridspace10 <jackovand27@icloud.com>
2024-03-28 23:35:32 +1000

    Added tables for songs in albums and songs in playlist, also modified songs table

--- CreateDB.sql
+++ CreateDB.sql
@@ -27,14 +27,30 @@ CREATE TABLE songs (
     albumid int NOT NULL, 
     FOREIGN KEY (albumid) REFERENCES album(id),
     DOR date NOT NULL,
+    genre text NOT NULL,
+    length int NOT NULL,
     artistid int NOT NULL,
     FOREIGN KEY (artistid) REFERENCES artist(id)
 )
 
 CREATE TABLE album (
     id PRIMARY KEY auto_increment NOT NULL,
-    name text NOT NULL,
+    name text NOT NULL, 
     DOC date NOT NULL,
     artistid int NOT NULL, 
     FOREIGN KEY (artistid) REFERENCES artist(id)
+)
+
+CREATE TABLE songs_albums(
+    albumid int NOT NULL,
+    songid int NOT NULL,
+    FOREIGN KEY (albumid) REFERENCES album(id),
+    FOREIGN KEY (songid) REFERENCES songs(id)
+)
+
+CREATE TABLE songs_playlist(
+    playlistid int NOT NULL,
+    songid int NOT NULL,
+    FOREIGN KEY (playlistid) REFERENCES playlist(id),
+    FOREIGN KEY (songid) REFERENCES songs(id)
 )
